ed16923f87d647c6f3927ea730822b71f8246e5d
Mahad444 <mahadsaid95@icloud.com>
2022-08-23 22:32:25 +0300

    react

---
+++ App.js
@@ -0,0 +1,13 @@
+import Nyumbani from './components/Home';
+ 
+import './components/Home';
+
+function App() {
+  return (
+  <div className='app'>
+<Nyumbani/>
+  </div>
+    );
+}
+
+export default App;
